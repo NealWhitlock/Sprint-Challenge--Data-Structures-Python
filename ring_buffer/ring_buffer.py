2e102278b86d75e35d8417993b8c8ac25a74d48d
NealWhitlock <NealWhitlock@users.noreply.github.com>
2020-06-12 09:25:20 -0600

    ring buffer done and tested

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,17 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity  # Size of the buffer
+        self.storage = []  # list to store items
+        self.counter = 0  # counter set to 0
 
     def append(self, item):
-        pass
+        loc = (self.counter % self.capacity)  # index location for new item
+        if len(self.storage) == self.capacity:  # if the list is full
+            self.storage.pop(loc)  # remove item in the index location
+            self.storage.insert(loc, item)  # put new item in that spot
+        else:
+            self.storage.append(item)
+        self.counter += 1  # increment counter
 
     def get(self):
-        pass
+        return self.storage
